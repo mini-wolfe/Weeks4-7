107974b043c53310471b5db9d1e3ac659b741633
Maya Wolfe <miniwolfe2>
2025-02-06 15:50:07 -0500

    key down changes color as place holder

--- Assets/coding gym week 5/button.cs
+++ Assets/coding gym week 5/button.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class button : MonoBehaviour
+    
 {
+    SpriteRenderer spriterenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriterenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R)) 
+        { 
+            spriterenderer.color = Color.blue;
+        }
     }
 }
