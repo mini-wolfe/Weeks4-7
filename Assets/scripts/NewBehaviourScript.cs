57cccfddabfd0cbe47e11cc1b58cce45bfb7f93c
Maya Wolfe <miniwolfe2>
2025-02-06 14:10:16 -0500

    finished in class example

--- Assets/scripts/NewBehaviourScript.cs
+++ Assets/scripts/NewBehaviourScript.cs
@@ -16,7 +16,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        t += Time.detaTime;
+        t += Time.deltaTime;
         slider.value = t % slider.maxValue;
         
     }
