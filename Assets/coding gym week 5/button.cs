3a0a05c21e091bee300b21228cff3df19ac86904
MINIS-PC\miniw <miniwolfe2@gmail.com>
2025-02-14 17:24:41 -0500

    the code wasn't working from last weeks repo, which had previously stooped working . i dont understand why it stoped working it worked fine in class i dont understand why it stoped , the error is resloved , i commented  out my scripted.

--- Assets/coding gym week 5/button.cs
+++ Assets/coding gym week 5/button.cs
@@ -9,15 +9,15 @@ public class button : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spriterenderer = GetComponent<SpriteRenderer>();
+       // spriterenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown() 
-        { 
-            spriterenderer.color = new Color(1,0.5f,0.3f,0.5f);
-        }
+       // if (Input.GetButtonDown() 
+       // { 
+            //spriterenderer.color = new Color(1,0.5f,0.3f,0.5f);
+       // }
     }
 }
