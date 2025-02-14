ee6c20a079738a41374328de53fb66ac7ac12418
MINIS-PC\miniw <miniwolfe2@gmail.com>
2025-02-14 17:21:00 -0500

    fixing git and visual studios

--- Assets/gym 6/behave.cs
+++ Assets/gym 6/behave.cs
@@ -7,7 +7,7 @@ public class behave : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
