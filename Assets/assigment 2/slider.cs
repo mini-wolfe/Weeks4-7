6d8689959abbce4315cf7feac68d59a42394d8e5
MINIS-PC\miniw <miniwolfe2@gmail.com>
2025-02-19 01:17:44 -0500

    adding in slider function using replacment sprites as stand in so that when the slider is pushed the tomagachi "skin" switches between the replacements

--- Assets/assigment 2/slider.cs
+++ Assets/assigment 2/slider.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class slider : MonoBehaviour
 {
+    //creating a referance to a sprite 
+    public SpriteRenderer sr;
+    //array list holding  all the diffrent tomagachi skins
+    public Sprite[] skins;
+    //
+    public Slider moving;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    //creating a function that changes the skin of the tomagachi when  the slider is pushed 
+    public void colourShift()
+    {
+        int sValue = Mathf.RoundToInt(moving.value);
+        sr.sprite = skins[sValue];
     }
 }
