c15f812030bafa186511dac190857d8f0c8dbb5c
Maya Wolfe <miniwolfe2@gmail.com>
2025-02-19 21:43:36 -0500

    apply the prefab code to the 3 prefabs then seperating then applying the code to the component in unity

--- Assets/assigment 2/spawn.cs
+++ Assets/assigment 2/spawn.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEditor;
 
 public class spawn : MonoBehaviour
 {
+  
     //creating a call to the prefab
     public GameObject interactionA;
+    public GameObject interactionB;
+    public GameObject interactionC;
+   
     //creating a new vector 3 
-    Vector3 position = new Vector3(0,1,0);
-    // Start is called before the first frame update
+    Vector3 positionA = new Vector3(0,1,0);
+    Vector3 positionB = new Vector3(0,3, 0);
+    Vector3 positionC = new Vector3(1, 0, 0);
+
+
+
+
     void Start()
     {
         
@@ -20,11 +31,29 @@ public class spawn : MonoBehaviour
         
     }
    
-    public void buttonPressed()
+    public void buttonPressedA()
     {
         //this code spawn the first item for the tomagachi to interact with. it is  set to the screen size of  a place holder,negating rotation with quaternion identity
-        Instantiate(interactionA,position,Quaternion.identity);
+        Instantiate(interactionA,positionA,Quaternion.identity);
         //destroy the prephab after 3 seconds
         Destroy(interactionA,3);
+       
+        
+       
+    }
+    public void ButtonPressedB()
+    {
+        //this code spawn the first item for the tomagachi to interact with. it is  set to the screen size of  a place holder,negating rotation with quaternion identity
+        Instantiate(interactionB, positionB, Quaternion.identity);
+        //destroy the prephab after 3 seconds
+       Destroy(interactionB, 3);
+
+    }
+    public void ButtonaPressedC()
+    {
+        //this code spawn the first item for the tomagachi to interact with. it is  set to the screen size of  a place holder,negating rotation with quaternion identity
+        Instantiate(interactionC, positionC, Quaternion.identity);
+        //destroy the prephab after 3 seconds
+        Destroy(interactionC, 3);
     }
 }
