fddba6f9a08c3348b2d32810b121000728f0b331
Maya Wolfe <miniwolfe2@gmail.com>
2025-02-19 22:39:26 -0500

    writing in code to count how many time the buttons have been clicked

--- Assets/assigment 2/text.cs
+++ Assets/assigment 2/text.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class text : MonoBehaviour
-{
+
+{//creating the couting int to create the promesh 
+    int count = 0;
+    public TextMeshProUGUI counter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // making it so that the timmerw ill not start out as blanc text 
+        counter.text = "times clicked " + count;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void counting() 
+    {//creating void function so that when buttons clicked the counter goes up 
+        count += 1;
+        counter.text ="times clicked " + count;
+       
     }
 }
